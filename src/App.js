d7deeadf62d7ea154c342c35c066018aaddb065e
Otabek Yuldashev <otabekyuldashev@Otabeks-iMac.local>
2024-02-04 20:27:28 -0700

    commented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import CounterPage from './components/CounterPage';
 import Book from './components/BookPage';
 import React from 'react';
-
+// new comment 
 function App() {
   return (
     <div className="App">
